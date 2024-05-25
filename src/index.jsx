fc81f96b4ee0d5da3430c75b0d84dbc69d5be5bb
CrisGMO <cristiancampos1508@gmail.com>
2024-05-25 10:34:43 -0300

    add analitycs

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React, { StrictMode, useState } from "react";
 import ReactDOM from 'react-dom/client';
 import lang from './lang.json';
 import Slider from './Components/Slider/Slider'
+import { Analytics } from "@vercel/analytics/react"
 
 var langActual = '';
 langActual = localStorage.getItem('clave') != null ? localStorage.getItem('clave') : localStorage.setItem('lang', 'es');
@@ -11,6 +12,7 @@ const rootConocimientos = ReactDOM.createRoot(document.getElementById('conocimie
 rootConocimientos.render(
     <StrictMode>
         <Slider></Slider>
+        <Analytics></Analytics>
     </StrictMode>
 )
 
